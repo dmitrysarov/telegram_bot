ba9b7596df141a459e7772dbf8be368531fa73fd
d.shaulsky <d.shaulsky@millionagents.com>
2018-04-04 19:37:49 +0300

    add url

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,8 +11,18 @@ VK_URL = 'https://api.vk.com/method/wall.get?v=5.52&domain=USER_NAME&access_toke
 @click.option('--user_name', help='vk user name to follow', required=True, multiple=True)
 @click.option('--v_token', help='vk app token', required=True)
 def main(t_token, channel, user_name, v_token):
-    print(t_token, channel, user_name, v_token)
     posts_id = set([])
+    link = ''
+    #initial scan of wall
+    for un in user_name:
+        req_ans = requests.get(VK_URL.replace('USER_NAME', un).replace('TOKEN', v_token)).content
+        vk_answer = json.loads(req_ans)
+        for example in vk_answer['response']['items']:
+            unique_id = '{}_{}_{}'.format(example['id'], example['from_id'], example['owner_id'])
+            if unique_id not in posts_id:
+                posts_id.add(unique_id)
+    print(posts_id)
+    #starting trace
     while True:
         for un in user_name:
             req_ans = requests.get(VK_URL.replace('USER_NAME', un).replace('TOKEN', v_token)).content
@@ -21,6 +31,14 @@ def main(t_token, channel, user_name, v_token):
                 unique_id = '{}_{}_{}'.format(example['id'], example['from_id'], example['owner_id'])
                 if unique_id not in posts_id:
                     requests.get(TELEGRAM_URL.replace('BOT_TOKEN', t_token).replace('CHAT_ID', channel).replace('TEXT', example['text']))
+                    for atch in example['attachments']:
+                        if atch['type'] == 'photo':
+                            phts = atch['photo']
+                            dim = [int(d.replace('photo_','')) for d in phts.keys() if d.startswith('photo_')]
+                            link = phts['photo_{}'.format(max(dim))]
+                    if link != '':
+                        requests.get(TELEGRAM_URL.replace('BOT_TOKEN', t_token).replace('CHAT_ID', channel).replace('TEXT', link))
+                        link = ''
                     posts_id.add(unique_id)
         time.sleep(20)
 if __name__ == '__main__':
